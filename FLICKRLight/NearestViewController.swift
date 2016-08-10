23475366fcea2a507746267953898ccfc45062dd
Yuriy Velichko <velichko.yuriy@gmail.com>
2016-08-10 17:05:37 +0300

    the segue to show picture detail was added

--- FLICKRLight/NearestViewController.swift
+++ FLICKRLight/NearestViewController.swift
@@ -47,16 +47,25 @@ class NearestViewController: UICollectionViewController, UICollectionViewDelegat
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func collectionView( collectionView: UICollectionView,
+                                   didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        performSegueWithIdentifier( "showDetail", sender:self )
+    }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
+        
+        if  let detailView = segue.destinationViewController as? DetailViewController,
+            let indexPath = collectionView?.indexPathsForSelectedItems()?.first {
+            
+        }
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
