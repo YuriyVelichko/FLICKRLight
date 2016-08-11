00864cb6428a459bfb053146f8d88a8bb14b5cb3
Yuriy Velichko <velichko.yuriy@gmail.com>
2016-08-11 15:45:14 +0300

    support gestures in the detail view

--- FLICKRLight/DetailViewController.swift
+++ FLICKRLight/DetailViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    private var scale               = CGFloat(1)
+    private var previousScale       = CGFloat(1)
+    private var rotation            = CGFloat(0)
+    private var previousRotation    = CGFloat(0)
+    
     var data : NSData?
 
     override func viewDidLoad() {
@@ -21,6 +27,26 @@ class DetailViewController: UIViewController {
         if let internalData = data {
             imageView.image = UIImage( data: internalData )
         }
+        
+        imageView.userInteractionEnabled = true
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(doPinch) )
+        pinchGesture.delegate = self
+        imageView.addGestureRecognizer(pinchGesture)
+        
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(doRotate) )
+        rotationGesture.delegate = self
+        imageView.addGestureRecognizer(rotationGesture)
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target:self, action:#selector(panGestureDetected) )
+        panGestureRecognizer.delegate = self
+        imageView.addGestureRecognizer(panGestureRecognizer)
+        
+        let doubleTouchRecognizer = UITapGestureRecognizer( target:self, action:#selector(doubleTouchDetected))
+        doubleTouchRecognizer.delegate = self
+        doubleTouchRecognizer.numberOfTapsRequired = 2
+        imageView.addGestureRecognizer( doubleTouchRecognizer )
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,5 +64,69 @@ class DetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func gestureRecognizer( gestureRecognizer: UIGestureRecognizer,
+                            shouldRecognizeSimultaneouslyWithGestureRecognizer
+                            otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func transformImageView() {
+        var transorm = CGAffineTransformMakeScale(scale * previousScale, scale * previousScale)
+        transorm = CGAffineTransformRotate(transorm, rotation + previousRotation)
+        imageView.transform = transorm
+    }
+    
+    func doPinch(gesture:UIPinchGestureRecognizer) {
+        
+        scale = gesture.scale
+        transformImageView()
+        
+        if gesture.state == .Ended {
+            previousScale = scale * previousScale
+            scale = 1
+        }
+    }
+    
+    func doRotate(gesture:UIRotationGestureRecognizer) {
+        
+        rotation = gesture.rotation
+        transformImageView()
+        
+        if gesture.state == .Ended {
+            previousRotation = rotation + previousRotation
+            rotation = 0
+        }
+    }
+    
+    var originalCenter = CGPoint()
+
+    func panGestureDetected( gesture : UIPanGestureRecognizer )
+    {
+        if previousScale <= 1 {
+            return
+        }
+        
+        if (gesture.state == .Began) {
+            originalCenter = gesture.view!.center;
+        } else if (gesture.state == .Changed) {
+            let translate = gesture.translationInView(gesture.view)
+            gesture.view!.center = CGPointMake(originalCenter.x + translate.x + 10, originalCenter.y + translate.y + 10 );
+        }
+    }
+
+    func doubleTouchDetected( gesture : UIPanGestureRecognizer )
+    {
+        UIView.animateWithDuration(0.3, animations: {
+            
+            self.scale               = CGFloat(1)
+            self.previousScale       = CGFloat(1)
+            self.rotation            = CGFloat(0)
+            self.previousRotation    = CGFloat(0)
+
+            gesture.view!.center = self.view.center
+            self.imageView.transform = CGAffineTransformIdentity
+        })
+    }
 
 }
