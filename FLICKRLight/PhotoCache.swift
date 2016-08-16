b973b95e5d27b2b008feb39fe2f89e7922fc81d3
Yuriy Velichko <velichko.yuriy@gmail.com>
2016-08-16 10:32:36 +0300

    refactoring of cache and downloader

--- FLICKRLight/PhotoCache.swift
+++ FLICKRLight/PhotoCache.swift
@@ -11,51 +11,15 @@ import AlamofireImage
 
 class PhotoCache {
     
-    private let downloader  : ImageDownloader
-    private let cache       : AutoPurgingImageCache
+    
 
     
     // MARK: - initializer
     
-    init()
+    init( cache : AutoPurgingImageCache )
     {
-        cache = AutoPurgingImageCache(
-            memoryCapacity: 100 * 1024 * 1024,
-            preferredMemoryUsageAfterPurge: 60 * 1024 * 1024
-        )
-        
-        downloader = ImageDownloader(
-            configuration: ImageDownloader.defaultURLSessionConfiguration(),
-            downloadPrioritization: .FIFO,
-            maximumActiveDownloads: 4,
-            imageCache: cache
-        )
-    }
+            }
     
     // MARK: - methods
     
-    func updateImage( url: NSURL, completion: (image : UIImage?) -> Void )
-    {
-        let URLRequest = NSURLRequest( URL: url )
-        
-        if let cachedImage = imageForURL( url ) {
-            completion( image: cachedImage )
-        } else {
-            downloader.downloadImage(URLRequest: URLRequest ) { response in
-                if let image = response.result.value {
-                    completion( image: image )
-                }
-            }
-        }
-    }
-    
-    func imageForURL( url: NSURL ) -> UIImage? {
-        
-        let URLRequest = NSURLRequest( URL: url )
-        return cache.imageForRequest( URLRequest )
-    }
-    
-    func clearCache() {
-        cache.removeAllImages()
-    }
-}
+   }
